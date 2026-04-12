6500da21bec84260c926030b7e2c97dcd554d5b1
안시연 <ansiyeon@ansiyeon-ui-MacBookAir.local>
2026-04-12 23:34:03 +0900

    fix: 불필오한 폴더 제거 및 gitignore 정리

--- Exception/src/main/java/com/demo/gp/repository/GroupPurchaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.gp.repository;
-
-import com.demo.gp.entity.GroupPurchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupPurchaseRepository extends JpaRepository<GroupPurchase, Integer> {
-}
